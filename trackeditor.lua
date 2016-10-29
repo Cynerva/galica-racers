b944bb7f972867f3596345e3ef959749a9e3738c
George Kraft <cynerva@gmail.com>
2016-10-29 17:35:20 -0500

    add editing controls to track editor

--- trackeditor.lua
+++ trackeditor.lua
@@ -1,14 +1,23 @@
 game.trackEditor = {}
 
-local posX = 0
-local posY = 0
+-- tile coordinates
+local posX = 1
+local posY = 1
+local finished = game.event.new()
 
 local function init()
   game.track.init()
-  game.camera.setPosition(game.tiles.worldPos(0, 0))
+  game.camera.setPosition(game.tiles.worldPos(posX, posY))
 end
 
 local function update()
+  if love.keyboard.isDown("1") then
+    game.tiles.setTile(posX, posY, game.tiles.dirt)
+  elseif love.keyboard.isDown("2") then
+    game.tiles.setTile(posX, posY, game.tiles.mud)
+  elseif love.keyboard.isDown("3") then
+    game.tiles.setTile(posX, posY, game.tiles.rock)
+  end
 end
 
 local function draw()
@@ -17,6 +26,19 @@ local function draw()
 end
 
 local function keypressed(key)
+  if key == "up" then
+    posY = posY - 1
+  elseif key == "down" then
+    posY = posY + 1
+  elseif key == "left" then
+    posX = posX - 1
+  elseif key == "right" then
+    posX = posX + 1
+  elseif key == "escape" then
+    finished:send()
+    return
+  end
+  game.camera.setPosition(game.tiles.worldPos(posX, posY))
 end
 
 function game.trackEditor.run()
@@ -24,5 +46,5 @@ function game.trackEditor.run()
   love.update = update
   love.draw = draw
   love.keypressed = keypressed
-  game.event.new():wait()
+  finished:wait()
 end
