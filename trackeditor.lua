8a27cc9d538a9439befa60544710f3d3ecd56c5a
George Kraft <cynerva@gmail.com>
2016-11-04 16:32:14 -0500

    reimplement UI split, margin

--- trackeditor.lua
+++ trackeditor.lua
@@ -2,6 +2,13 @@ game.trackEditor = {}
 
 local finished = game.event.new()
 
+local ui = game.ui.split("root", 2/3,
+  game.ui.pane("track"),
+  game.ui.margin("sidePanel", 10,
+    game.ui.pane("button")
+  )
+)
+
 local function update()
 end
 
@@ -12,15 +19,34 @@ local function keypressed(key)
   end
 end
 
+local function mousepressed(x, y)
+  if game.ui.contains(ui, "button", x, y) then
+    game.terrain.cycleBase()
+  end
+end
+
 local function draw()
-  game.camera.transform()
-  game.track.draw()
+  game.ui.run(ui, {
+    track=function()
+      game.camera.transform()
+      game.track.draw()
+    end,
+    sidePanel=function()
+      love.graphics.setColor(192, 192, 192)
+      love.graphics.rectangle("fill", 0, 0, game.ui.width, game.ui.height)
+    end,
+    button=function()
+      love.graphics.setColor(0, 0, 0)
+      love.graphics.print("Cycle terrain")
+    end
+  })
 end
 
 function game.trackEditor.run()
   love.update = update
-  love.draw = draw
   love.keypressed = keypressed
+  love.mousepressed = mousepressed
+  love.draw = draw
   game.track.load()
   finished:wait()
   game.track.save()
--- ui.lua
+++ ui.lua
@@ -1,9 +1,93 @@
 game.ui = {}
 
+game.ui.x = 0
+game.ui.y = 0
 game.ui.width = love.graphics.getWidth()
 game.ui.height = love.graphics.getHeight()
 
 function love.resize(w, h)
   game.ui.width = w
   game.ui.height = h
+end
+
+function game.ui.run(ui, callbacks)
+  local id = ui.id
+  local f = callbacks[id]
+  if f ~= nil then
+    f()
+  end
+  ui.runChildren(callbacks)
+end
+
+local function runChild(ui, callbacks, x, y, w, h)
+  local originalX = game.ui.x
+  local originalY = game.ui.y
+  local originalWidth = game.ui.width
+  local originalHeight = game.ui.height
+  game.ui.x = game.ui.x + x
+  game.ui.y = game.ui.y + y
+  game.ui.width = w
+  game.ui.height = h
+  love.graphics.push()
+  love.graphics.translate(x, y)
+  game.ui.run(ui, callbacks)
+  love.graphics.pop()
+  game.ui.x = originalX
+  game.ui.y = originalY
+  game.ui.width = originalWidth
+  game.ui.height = originalHeight
+end
+
+function game.ui.contains(ui, id, x, y)
+  local result = false
+  game.ui.run(ui, {[id] = function()
+    if x >= game.ui.x and x < game.ui.x + game.ui.width and y >= game.ui.y and y < game.ui.y + game.ui.height then
+      result = true
+    end
+  end})
+  return result
+end
+
+function game.ui.pane(id)
+  return {id=id, runChildren=function(callbacks) end}
+end
+
+function game.ui.splitHorizontal(id, weight, left, right)
+  return {id=id, runChildren=function(callbacks)
+    local w = game.ui.width
+    local h = game.ui.height
+    runChild(left, callbacks, 0, 0, w * weight, h)
+    runChild(right, callbacks, w * weight, 0, w * (1 - weight), h)
+  end}
+end
+
+function game.ui.splitVertical(id, weight, top, bottom)
+  return {id=id, runChildren=function(callbacks)
+    local w = game.ui.width
+    local h = game.ui.height
+    runChild(top, callbacks, 0, 0, w, h * weight)
+    runChild(bottom, callbacks, 0, h * weight, w, h * (1 - weight))
+  end}
+end
+
+function game.ui.split(id, weight, a, b)
+  return {id=id, runChildren=function(callbacks)
+    local w = game.ui.width
+    local h = game.ui.height
+    if w > h then
+      runChild(a, callbacks, 0, 0, w * weight, h)
+      runChild(b, callbacks, w * weight, 0, w * (1 - weight), h)
+    else
+      runChild(a, callbacks, 0, 0, w, h * weight)
+      runChild(b, callbacks, 0, h * weight, w, h * (1 - weight))
+    end
+  end}
+end
+
+function game.ui.margin(id, size, child)
+  return {id=id, runChildren=function(callbacks)
+    local w = game.ui.width
+    local h = game.ui.height
+    runChild(child, callbacks, size, size, w - size * 2, h - size * 2)
+  end}
 end
