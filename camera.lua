ec12e24eac8800fec45d0a8956b9f4e855ce382b
George Kraft <cynerva@gmail.com>
2016-11-12 16:04:06 -0600

    adjust camera zoom based on screen size

--- camera.lua
+++ camera.lua
@@ -3,7 +3,6 @@ game.camera = {}
 local followedBody = nil
 local posX = 0
 local posY = 0
-local zoom = 20
 
 local function pos()
   if followedBody then
@@ -14,6 +13,11 @@ local function pos()
     return x, y
   end
   return posX, posY
+end
+
+local function getZoom()
+  local size = (game.ui.width + game.ui.height) / 2
+  return size / 50
 end
 
 function game.camera.followCar()
@@ -33,12 +37,14 @@ end
 function game.camera.transform()
   local x, y = pos()
   love.graphics.translate(game.ui.width / 2, game.ui.height / 2)
+  local zoom = getZoom()
   love.graphics.scale(zoom, zoom)
   love.graphics.translate(-x, -y)
 end
 
 function game.camera.screenToWorld(x, y)
   local camX, camY = pos()
+  local zoom = getZoom()
   x = camX + (x - game.ui.width / 2) / zoom
   y = camY + (y - game.ui.height / 2) / zoom
   return x, y
